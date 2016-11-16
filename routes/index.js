0e542d38a0aafb92c666bb45d03efcec572517f9
LNoe-lzy <m15222505516@163.com>
2016-11-16 16:10:27 +0800

    实现注册登录功能，创建sign模块,实现前端js文件的模块化加载。

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,69 @@
 var express = require('express');
 var router = express.Router();
+var crypto = require('crypto');
+
+var User = require('../models/user');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+  res.render('index', {
+    title: 'VIEW',
+    user: req.session.user
+  });
+});
+
+router.post('/signup', function (req, res) {
+  User.findOne({
+    email: req.body.email
+  }, function (err, u) {
+    if (u) {
+      req.flash('error', '用户已存在!');
+      res.redirect('/');
+    }
+    var email = req.body.email;
+    var name = req.body.name;
+    var password = req.body.password;
+    var passwordRep = req.body.passwordRep;
+    var md5 = crypto.createHash('md5');
+    if (password !== passwordRep) {
+      req.flash('error', '两次输入的密码不一致!');
+      res.redirect('/');
+    }
+    var user = new User({
+      name: name,
+      password: md5.update(password).digest('hex'),
+      email: email
+    });
+    user.save(function (err) {
+      if (err) {
+        console.log(err);
+      }
+      req.session.user = user;
+      res.redirect('/');
+    });
+  });
+});
+
+router.post('/signin', function (req, res) {
+  var password = crypto.createHash('md5').update(req.body.password).digest('hex');
+  User.findOne({
+    email: req.body.email
+  }, function (err, u) {
+    if (err) {
+      console.log(err);
+    }
+    if (!u || u.password !== password) {
+      req.flash('error', '用户名或密码错误！');
+      res.redirect('/');
+    }
+    req.session.user = u;
+    res.redirect('/');
+  })
+});
+
+router.get('/logout', function (req, res) {
+  req.session.user = null;
+  res.redirect('/');
 });
 
 module.exports = router;
