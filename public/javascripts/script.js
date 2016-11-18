49f4e8e90c63a0b3dc0c0df768051c68a09abd68
LNoe-lzy <m15222505516@163.com>
2016-11-18 14:04:44 +0800

    增加前端瀑布流布局并将其写入image模块，分离view模块。

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -4,92 +4,18 @@
 $(document).ready(function () {
     var view = new View();
     var viewModule = view.module();
-    viewModule.require(['sign'], function () {
-        new Sign();
+    viewModule.require(['sign', 'image'], function () {
+        new SignModule();
+        var imageModule = new ImageModule();
+
+        var imgTopArr = document.getElementsByClassName('img-top');
+        for (var i = 0, j = imgTopArr.length; i < j; i ++ ) {
+            (function (k) {
+                var path = imgTopArr[k].getAttribute('data-imgpath');
+                imageModule.proxy(imgTopArr[k], path);
+            })(i);
+        }
+
+        // imageModule.waterfull(document.getElementById('view-right'), document.getElementsByClassName('img-item'));
     });
 });
-
-
-var View = function () {
-    this.init();
-};
-
-View.prototype = {
-    init: function () {
-
-    },
-    module: function () {
-        var moduleMap = [];
-        var fileMap = [];
-        var noop = function () {};
-
-        var define = function (name, dep, fn) {
-            var module;
-            if (!moduleMap[name]) {
-                module = {
-                    name: name,
-                    dependencies: dep,
-                    fn: fn
-                };
-                moduleMap[name] = module;
-            }
-            return moduleMap[name];
-        };
-
-        var require = function (pathArr, callback) {
-            for (var i = 0; i < pathArr.length; i++) {
-                var path = pathArr[i];
-
-                if (!fileMap[path]) {
-                    var head = document.getElementsByTagName('head')[0];
-                    var script = document.createElement('script');
-                    script.type = 'text/javascript';
-                    script.async = 'true';
-                    script.src = '../javascripts/' + path + '.js';
-                    script.onload = function () {
-                        fileMap[path] = true;
-                        head.removeChild(script);
-                        checkAllFiles();
-                    };
-                    head.appendChild(script);
-                }
-            }
-
-            function checkAllFiles () {
-                var allLoaded = true;
-                for (var i = 0; i < pathArr.length; i++) {
-                    if (!fileMap[pathArr[i]]) {
-                        allLoaded = false;
-                        break;
-                    }
-                }
-                if (allLoaded) {
-                    callback();
-                }
-            }
-        };
-
-        var use = function (name) {
-            var module = moduleMap[name];
-
-            if (!module.entity) {
-                var args = [];
-                for (var i = 0; i < module.dependencies.length; i++) {
-                    if (moduleMap[module.dependencies[i]].entity) {
-                        args.push(moduleMap[module.dependencies[i]].entity);
-                    } else {
-                        args.push(this.use(module.dependencies[i]));
-                    }
-                }
-                module.entity = module.fn.apply(noop, args);
-            }
-            return module.entity;
-        };
-
-        return {
-            require: require,
-            use: use,
-            define: define
-        };
-    }
-};
--- public/javascripts/sign.js
+++ public/javascripts/sign.js
@@ -1,35 +1,38 @@
 /**
  * Created by lizongyuan on 2016/11/16.
  */
-var Sign = function () {
-    this.init();
-};
+;(function () {
+    var SignModule = function () {
+        this.init();
+    };
 
-Sign.prototype = {
-    init: function () {
-        this.addEvent('signin');
-        this.addEvent('signup');
-    },
-    addEvent: function (type) {
-        var clientH = document.documentElement.clientHeight;
-        var sign = document.getElementById(type);
-        if (sign) {
-            sign.addEventListener('click', function () {
-                document.getElementById('mask').style['display'] = 'block';
-                var signFrame = document.getElementById(type + 'Frame');
-                signFrame.style['display'] = 'block';
-                signFrame.style['top'] = (clientH - signFrame.clientHeight) / 2 + 'px';
-            });
-        }
-        var closeNode = document.getElementsByClassName('sign-close');
-        for (var i = 0, j = closeNode.length; i < j; i++ ) {
-            (function (k) {
-                closeNode[k].addEventListener('click', function () {
-                    document.getElementById('mask').style['display'] = 'none';
-                    document.getElementById('signupFrame').style['display'] = 'none';
-                    document.getElementById('signinFrame').style['display'] = 'none';
+    SignModule.prototype = {
+        init: function () {
+            this.addEvent('signin');
+            this.addEvent('signup');
+        },
+        addEvent: function (type) {
+            var clientH = document.documentElement.clientHeight;
+            var sign = document.getElementById(type);
+            if (sign) {
+                sign.addEventListener('click', function () {
+                    document.getElementById('mask').style['display'] = 'block';
+                    var signFrame = document.getElementById(type + 'Frame');
+                    signFrame.style['display'] = 'block';
+                    signFrame.style['top'] = (clientH - signFrame.clientHeight) / 2 + 'px';
                 });
-            })(i)
+            }
+            var closeNode = document.getElementsByClassName('sign-close');
+            for (var i = 0, j = closeNode.length; i < j; i++ ) {
+                (function (k) {
+                    closeNode[k].addEventListener('click', function () {
+                        document.getElementById('mask').style['display'] = 'none';
+                        document.getElementById('signupFrame').style['display'] = 'none';
+                        document.getElementById('signinFrame').style['display'] = 'none';
+                    });
+                })(i)
+            }
         }
-    }
-};
+    };
+    window['SignModule'] = SignModule;
+})();
